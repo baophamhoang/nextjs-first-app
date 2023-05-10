8679e54e8db7c7922a9be4bc0b324ea6dd272467
Bao Pham <bao_pham@datahouse.com>
2023-05-10 01:11:53 -1000

    cached full list before loading

--- src/pages/api/pokemon/[id].ts
+++ src/pages/api/pokemon/[id].ts
@@ -1,12 +1,9 @@
 import client from "@/apis/client";
 import { NamedAPIResourceList, Pokemon } from "@/types/pokemon";
+import { createRedisClient } from "@/utils/redis";
 import axios from "axios";
 import _ from "lodash";
 import { NextApiRequest, NextApiResponse } from "next";
-import { createClient } from "redis";
-
-const redisClient = createClient();
-redisClient.connect();
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   const { id } = req.query;
@@ -16,49 +13,63 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
   }
 
   // cached full name list and reuse
-  redisClient.on('error', err => console.log('Redis Client Error', err));
-  // await redisClient.connect();
+  let cachedNamedList: {[key: string]: string};
 
-  let namedList;
   try {
-    namedList = await redisClient.hGetAll('cachedNamedList');
-    console.log('namedList :>> ', namedList);
-    console.log('nameList :>> ', JSON.stringify(namedList));
-    if (_.isEmpty(namedList)) {
+    const redisClient = await createRedisClient();
+    cachedNamedList = await redisClient.hGetAll('cachedNamedList');
+    if (_.isEmpty(cachedNamedList)) {
       try {
         const namedListResponse = await client.get<NamedAPIResourceList>(
-          "https://pokeapi.co/api/v2/pokemon/?limit=200"
+          "https://pokeapi.co/api/v2/pokemon/?limit=10000"
         );
-        namedList = namedListResponse;
         const formattedResponse = namedListResponse.data.results.map(obj => Object.values(obj));
         for (const [key, value] of formattedResponse) {
-          console.log('[key, value] :>> ', [key, value]);
           await redisClient.hSet('cachedNamedList', key, value);
         }
+        cachedNamedList = await redisClient.hGetAll('cachedNamedList');
       } catch (e) {
         res.status(500).json({ message: "Can't get Pokemon list" });
       }
     }
-  } catch (e) {
-    res.status(500).json({ message: "Redis Client got error" });
+  } catch (error) {
+    console.error('Failed to connect to Redis:', error);
   }
+  
+  // try {
+  //   namedList = await redisClient.hGetAll('cachedNamedList');
+  //   if (_.isEmpty(namedList)) {
+  //     try {
+  //       const namedListResponse = await client.get<NamedAPIResourceList>(
+  //         "https://pokeapi.co/api/v2/pokemon/?limit=200"
+  //       );
+  //       namedList = namedListResponse;
+  //       const formattedResponse = namedListResponse.data.results.map(obj => Object.values(obj));
+  //       for (const [key, value] of formattedResponse) {
+  //         console.log('[key, value] :>> ', [key, value]);
+  //         await redisClient.hSet('cachedNamedList', key, value);
+  //       }
+  //     } catch (e) {
+  //       res.status(500).json({ message: "Can't get Pokemon list" });
+  //     }
+  //   }
+  // } catch (e) {
+  //   res.status(500).json({ message: "Redis Client got error" });
+  // }
 
   let response;
   const methodHandler: { [method: string]: Function } = {
     GET: async () => {
       try {
-        const apiResponse = await client.get<NamedAPIResourceList>(
-          "https://pokeapi.co/api/v2/pokemon/?limit=200"
-        );
-        response = apiResponse.data.results.filter((item) => {
-          const url = item.url.split("/");
+        response = Object.entries(cachedNamedList).filter((e) => {
+          const url = e[1].split("/");
           const isIdIncluded = (url.pop() || url.pop())?.includes(id);
-          const isNameIncluded = item.name.includes(id);
+          const isNameIncluded = e[0].includes(id);
           return isIdIncluded || isNameIncluded;
         });
         try {
           const responseList = await Promise.all(
-            response.map((r) => axios.get<Pokemon>(r.url))
+            response.map((r) => axios.get<Pokemon>(r[1]))
           );
           const dataList = responseList.map((r) => ({
             id: r.data.id,
