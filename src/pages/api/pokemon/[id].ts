bc37159d9c72702cfdfd50b6b08109b0cbceb239
Bao Pham <bao_pham@datahouse.com>
2023-05-27 12:01:36 +0700

    add redis to cached the full list of data

--- src/pages/api/pokemon/[id].ts
+++ src/pages/api/pokemon/[id].ts
@@ -1,41 +1,75 @@
 import client from "@/apis/client";
 import { NamedAPIResourceList, Pokemon } from "@/types/pokemon";
 import axios from "axios";
+import _ from "lodash";
 import { NextApiRequest, NextApiResponse } from "next";
+import { createClient } from "redis";
 
-export default function handler(
-  req: NextApiRequest,
-  res: NextApiResponse
-) {
+const redisClient = createClient();
+redisClient.connect();
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   const { id } = req.query;
   if (!id || Array.isArray(id)) {
     res.end([]);
     return;
   }
+
+  // cached full name list and reuse
+  redisClient.on('error', err => console.log('Redis Client Error', err));
+  // await redisClient.connect();
+  console.log('true');
+
+  let namedList;
+  try {
+    namedList = await redisClient.hGetAll('cachedNamedList');
+    console.log('namedList :>> ', namedList);
+    console.log('nameList :>> ', JSON.stringify(namedList));
+    if (_.isEmpty(namedList)) {
+      try {
+        const namedListResponse = await client.get<NamedAPIResourceList>(
+          "https://pokeapi.co/api/v2/pokemon/?limit=200"
+        );
+        namedList = namedListResponse;
+        const formattedResponse = namedListResponse.data.results.map(obj => Object.entries(obj)[0]);
+        console.log('formattedResponse :>> ', formattedResponse);
+        await redisClient.hSet('cachedNamedList', formattedResponse)
+      } catch (e) {
+        res.status(500).json({ message: "Can't get Pokemon list" });
+      }
+    }
+  } catch (e) {
+    res.status(500).json({ message: "Redis Client got error" });
+  }
+
   let response;
   const methodHandler: { [method: string]: Function } = {
     GET: async () => {
       try {
-        const apiResponse = await client.get<NamedAPIResourceList>('https://pokeapi.co/api/v2/pokemon/?limit=200');
-        response = apiResponse.data.results.filter(item => {
-          const url = item.url.split('/');
+        const apiResponse = await client.get<NamedAPIResourceList>(
+          "https://pokeapi.co/api/v2/pokemon/?limit=200"
+        );
+        response = apiResponse.data.results.filter((item) => {
+          const url = item.url.split("/");
           const isIdIncluded = (url.pop() || url.pop())?.includes(id);
           const isNameIncluded = item.name.includes(id);
           return isIdIncluded || isNameIncluded;
         });
         try {
-          const responseList = await Promise.all(response.map(r => axios.get<Pokemon>(r.url)));
+          const responseList = await Promise.all(
+            response.map((r) => axios.get<Pokemon>(r.url))
+          );
           const dataList = responseList.map((r) => ({
             id: r.data.id,
             name: r.data.name,
-            imgUrl: r.data.sprites.front_default
+            imgUrl: r.data.sprites.front_default,
           }));
-          res.end(JSON.stringify(dataList))
+          res.end(JSON.stringify(dataList));
         } catch (e) {
-          res.status(500).json({message: "Can't get Pokemon list"});
+          res.status(500).json({ message: "Can't get Pokemon list" });
         }
       } catch (error) {
-        res.status(500).json({message: "Can't get Pokemon list"});
+        res.status(500).json({ message: "Can't get Pokemon list" });
       }
     },
   };
