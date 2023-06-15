3bd323f805c762b5c684c071ecccc56fecc6bc37
Bao Pham <bao_pham@datahouse.com>
2023-06-14 18:34:11 -1000

    test deploy

--- src/apis/client.ts
+++ src/apis/client.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = process.env.API_URL || '/nextjs-first-app/api';
+const API_URL = process.env.API_URL || '/api';
 
 const client = axios.create({
   baseURL: API_URL,
