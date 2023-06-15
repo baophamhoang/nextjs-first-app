2f5dcd72caed193847c7f2877a874b34b7ed197b
Bao Pham <bao_pham@datahouse.com>
2023-06-14 18:25:19 -1000

    test2

--- src/apis/client.ts
+++ src/apis/client.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'https://baophamhoang.github.io/nextjs-first-app/api' || '/nextjs-first-app/api';
+const API_URL = process.env.API_URL || '/nextjs-first-app/api';
 
 const client = axios.create({
   baseURL: API_URL,
