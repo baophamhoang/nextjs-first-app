348a182970c3549745cb53cc2fcb20dc2dec99dc
Bao Pham <bao_pham@datahouse.com>
2023-06-19 21:12:28 -1000

    test

--- src/apis/client.ts
+++ src/apis/client.ts
@@ -1,6 +1,9 @@
 import axios from 'axios';
 
 const API_URL = process.env.API_URL || '/api';
+const VARS_API_URL = process.env.VARS_API_URL || '/api';
+console.log('process.env.API_URL :>> ', process.env.API_URL);
+console.log('process.env.VARS_API_URL :>> ',process.env.VARS_API_URL);
 
 const client = axios.create({
   baseURL: API_URL,
