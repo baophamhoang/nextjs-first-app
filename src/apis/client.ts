bf529740eb7f005b7adf3f0ad476911597761e4b
Bao Pham <bao_pham@datahouse.com>
2023-06-19 21:27:31 -1000

    tét

--- src/apis/client.ts
+++ src/apis/client.ts
@@ -2,6 +2,8 @@ import axios from 'axios';
 
 const API_URL = process.env.API_URL || '/api';
 const VARS_API_URL = process.env.VARS_API_URL || '/api';
+console.log('process.env.API_URL :>> ', process);
+console.log('process.env.API_URL :>> ', process.env);
 console.log('process.env.API_URL :>> ', process.env.API_URL);
 console.log('process.env.VARS_API_URL :>> ',process.env.VARS_API_URL);
 
